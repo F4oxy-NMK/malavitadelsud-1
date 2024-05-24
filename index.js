b68e19a96be343a502306178e9ecffb14be31c39
F4oxy-NMK <148622603+F4oxy-NMK@users.noreply.github.com>
2024-05-24 15:44:28 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["discord.gg/malavita-del-sud","Malavita Del Sud Bot"];
 
 
 let currentIndex = 0;
